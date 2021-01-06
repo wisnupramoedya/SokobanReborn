c12b4b7094d11b7df77f00f03da7ac04b460ff96
Jagad Wijaya Purnomo <Jagadpurnomo26@gmail.com>
2021-01-06 18:12:19 +0700

    Add game classes & panel

---
+++ src/com/plotarmor/game/Baggage.java
@@ -0,0 +1,29 @@
+package com.plotarmor.game;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Baggage extends Actor {
+
+    public Baggage(int x, int y) {
+        super(x, y);
+        
+        initBaggage();
+    }
+    
+    private void initBaggage() {
+        
+        ImageIcon iicon = new ImageIcon("src/resources/box.png");
+        Image image = iicon.getImage().getScaledInstance(35, 35, Image.SCALE_DEFAULT);
+        setImage(image);
+    }
+
+    public void move(int x, int y) {
+        
+        int dx = x() + x;
+        int dy = y() + y;
+        
+        setX(dx);
+        setY(dy);
+    }
+}
